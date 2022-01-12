c4d733329c475a720e82765747821068c1d8b7ba
Stephen Murphy <stephendpmurphy@msn.com>
2022-01-12 14:19:30 -0500

    Initial commit

---
+++ src/board/board.c
@@ -0,0 +1,37 @@
+/******************************************************************************
+Glassboard Standard Board Init
+Copyright (c) 2021 Glassboard
+board.c
+*******************************************************************************/
+#include <stdint.h>
+
+volatile uint32_t g_systickCounter;
+volatile uint32_t g_delayCounter;
+
+// SysTick Handler function assumes that a tick is every 1ms
+void SysTick_Handler(void)
+{
+    if (g_delayCounter != 0U)
+    {
+        g_delayCounter--;
+    }
+
+    g_systickCounter++;
+}
+
+void BOARD_Init(void) {
+    // Clock init
+    // Peripheral Init
+}
+
+void BOARD_DelayTicks(uint32_t n)
+{
+    g_delayCounter = n;
+    while (g_delayCounter != 0U)
+    {
+    }
+}
+
+uint32_t BOARD_GetTick(void) {
+    return g_systickCounter;
+}
---
+++ src/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "error.h"
+#include "board.h"
+
+void main(void) {
+    app_return_code_t ret = APP_RET_OK;
+
+    // Initialze the board peripherals
+    BOARD_Init();
+
+    // Other init code
+
+    // Start running the application code
+    while(1) {
+        BOARD_DelayTicks(1000);
+    }
+}
